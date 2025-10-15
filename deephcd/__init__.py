be1457de6ce74989ab6e277133dbddad3deede1c
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2025-10-15 09:44:06 -0700

    changes to build files to address incorrect package build

--- deephcd/__init__.py
+++ deephcd/__init__.py
@@ -1,6 +1,6 @@
-import utils as utils
-import model as model
-import simulate as simulate
+from . import utils
+from . import model
+from . import simulate
 
 # export explicitly for better readability and to avoid circular imports
 __all__ = ["utils", "model", "simulate"]
